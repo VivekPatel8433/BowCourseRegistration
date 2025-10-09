e4fa071709a72bf497a366e2aa7d207fa6cb2fe3
Vivekbhai Patel <VivekPatel8433@gmail.com>
2025-10-09 13:31:15 -0600

    Files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,16 @@
-import {React} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import View from "./view/view";
-
-// Temporary placeholder pages
-function Home() {
-  return <h1 className="text-center text-2xl py-10">Home Page</h1>;
-}
-function CreateAccount() {
-  return <h1 className="text-center text-2xl py-10">Create Account</h1>;
-}
-function Admin() {
-  return <h1 className="text-center text-2xl py-10">Admin Dashboard</h1>;
-}
-function Student() {
-  return <h1 className="text-center text-2xl py-10">Student Dashboard</h1>;
-}
+import Home from "./pages/HomePage";
+import CreateAccount from "./pages/CreateAccount";
+import Admin from "./pages/AdminDashboard";
+import Student from "./pages/StudentDashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<View />}>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/create-account" element={<CreateAccount />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/student" element={<Student />} />
--- my-app/src/pages/HomePage.jsx
+++ my-app/src/pages/HomePage.jsx
@@ -3,3 +3,11 @@
 // - View all courses
 // - Button to go to Signup
 // - Button/link to Login (can be modal or separate page)
+
+function Home() {
+  return ( 
+  <h1 className="text-center text-2xl py-10">Home Page</h1> 
+  );
+}
+
+export default Home;
