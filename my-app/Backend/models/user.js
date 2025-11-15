f0cc59aa11d03748665be51624e032511eeb59ff
Vivekbhai Patel <VivekPatel8433@gmail.com>
2025-11-14 19:12:23 -0700

    Models

--- my-app/Backend/models/user.js
+++ my-app/Backend/models/user.js
@@ -1,3 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.
+const userSchema = mongoose.Schema({
+    // Common
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+
+    // Student specific
+  role: {type: String, enum: ['student', 'admin'], required: true},
+  studentData: {
+    phoneNumber: String,
+    birthday: Date,
+    department: String,
+    program: String
+  },
+  createdAt: { type: Date, default: Date.now }
+})
+
+export default userSchema;
