9da09c7a7053e183d9b0dd42e72d0bc489a02ed7
Carlo David <carlosicatdavid@gmail.com>
2025-11-26 00:38:28 -0700

    Update Files

--- Backend/server.js
+++ Backend/server.js
@@ -3,33 +3,38 @@ import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import router from "./routes/authRoutes.js";
 import cors from "cors";
-import courseRouter from './routes/courseRoutes.js';
-import programRouter from './routes/programRoutes.js';
+import courseRouter from "./routes/courseRoutes.js";
+import programRouter from "./routes/programRoutes.js";
+import studentRouter from "./routes/studentRoutes.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors({
-  origin: " http://localhost:3001", 
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: " http://localhost:3001",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 // Connect to MongoDB
 connectDB();
 
+app.use(cookieParser());
 app.use("/api/auth", router);
-app.use('/api/courses', courseRouter);
-app.use('/api/programs', programRouter);
-
+app.use("/api/courses", courseRouter);
+app.use("/api/programs", programRouter);
+app.use("/api/students", studentRouter);
 
 // Example route
 app.get("/", (req, res) => {
-    res.send("API running...");
+  res.send("API running...");
 });
-
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
